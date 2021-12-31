0d3c1bc28683c40d0ae3266b2ed423666ac9f3e1
O. Qudah <69857856+BasicPixel@users.noreply.github.com>
2021-12-31 09:38:07 +0200

    Fix CORS error by adding django-cors-headers

--- iffa_quotes/settings.py
+++ iffa_quotes/settings.py
@@ -42,12 +42,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -133,6 +135,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ]
 }
+
+CORS_ORIGIN_WHITELIST = ['localhost:3000']
 
 # Configure Django App for Heroku.
 django_on_heroku.settings(locals())
